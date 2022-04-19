436a8eb491c4630af4e5806f5e8681cf6280242a
piyush-qumar <70861172+piyush-qumar@users.noreply.github.com>
2022-04-19 22:35:31 +0530

    Time: 63 ms (79.24%), Space: 33.3 MB (99.74%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -5,10 +5,9 @@ public:
         result.reserve(nums.size());
         
         for(int i = 0 ; i < nums.size() ; ++i){
-           
-               
-            nums[abs(nums[i])-1] = -1*(nums[abs(nums[i])-1]);
-                
+            
+            nums[abs(nums[i])-1] = -(nums[abs(nums[i])-1]);
+            
             if(nums[abs(nums[i])-1] >0){
                 result.push_back(abs(nums[i]));
             }
