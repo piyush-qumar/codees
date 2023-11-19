4efe4fea3723575351ed14bf624d379be7705fe9
Piyush Kumar <piyushkumar5271@gmail.com>
2023-11-19 09:44:23 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (25.00%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        for (int i = 0; i < numRows; i++) {
-            vector<int> row(i + 1, 1);
-            for (int j = 1; j < i; j++) {
-                row[j] = res[i - 1][j - 1] + res[i - 1][j];
-            }
-            res.push_back(row);
+    vector<int>generateRow(int N)
+    {
+        long long res = 1;
+        vector<int>vec;
+        vec.push_back(1);
+        for(int i=1;i<N;i++)
+        {
+            res = res*(N-i);
+            res = res/i;
+            vec.push_back(res);
         }
-        return res;
+        return vec;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>result;
+        for(int i=1;i<=numRows;i++) {
+        result.push_back(generateRow(i));
+        }
+        return result;
     }
 };
