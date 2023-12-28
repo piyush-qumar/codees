0ee5f47700b65c11760fce25ba296eae28821b20
Piyush Kumar <piyushkumar5271@gmail.com>
2023-12-28 16:42:08 +0530

    Time: 1856 ms (28.12%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -19,5 +19,5 @@ from Students s
 cross join Subjects sub
 left join Examinations e 
 on s.student_id = e.student_id and sub.subject_name = e.subject_name
-group by s.student_id, sub.subject_name, s.student_name
+group by s.student_id, sub.subject_name
 ORDER BY s.student_id, sub.subject_name;
